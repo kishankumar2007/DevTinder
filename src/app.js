5533c6ad7f1b1e432811d0a2842c8f7bb5f67b93
mrperfectVip <kk7012234@gmail.com>
2025-06-19 11:16:28 +0530

    multiple-route-handler

--- src/app.js
+++ src/app.js
@@ -1,13 +1,28 @@
-const express = require('express')
-require('dotenv').config()
+const express = require("express");
+require("dotenv").config();
 
-const app = express()
-const PORT = process.env.PORT||3500
+const app = express();
+const PORT = process.env.PORT || 3500;
 
-app.use('/',(req,res)=>{
-    res.send("Welcome to Home Side...")
-})
+app
+  .get(
+    "/user",
+    (req, res, next) => {
+      console.log("First res send..");
+      next();
+      //res.send("Hello World") //we will get an error
+    },
+    (req, res, next) => {
+      console.log("response from the 2nd route handler");
+      //res.send("response from the 2nd route handler")
+      next(); //error cannot get /user
+    }
+  )
+  .post("/user", (req, res, next) => {
+    res.send("Res from POST");
+    next();
+  });
 
-app.listen(PORT,()=>{
-    console.log(`Server is listing on ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`Server is listing on ${PORT}`);
+});
