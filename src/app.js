2bd11ea3334d9cb7b2efb346bba5abd1d9513b79
mrperfectVip <kk7012234@gmail.com>
2025-06-20 16:20:22 +0530

    created-api-and-performed-CURD-Operation

--- src/app.js
+++ src/app.js
@@ -5,17 +5,69 @@ require("dotenv").config();
 const app = express();
 const PORT = process.env.PORT || 3500;
 
-const userData = {
-  firstName: "Kishan",
-  lastName: "Kumar",
-  email: "kishan2@gmail.com",
-  password: "Kishan@098",
-  age: 21,
-  gender: "male",
-};
+app.use(express.json());
+
+app.get("/feed", async (req, res) => {
+  try {
+    const user = await User.find(req.body);
+    if (user.length === 0) res.status(404).send("No user found");
+    res.send(user);
+  } catch (error) {
+    res.status(400).send(error.message);
+  }
+});
+
+app.delete("/user", async (req, res) => {
+  try {
+    console.log(req.body.userId);
+    const user = await User.findByIdAndDelete(req.body.userId);
+    console.log(user);
+    if (user) {
+      res.send("User has been deleted succesfully...");
+    } else {
+      throw new Error("No user found");
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).send(error.message);
+  }
+});
+
+app.patch("/update", async (req, res) => {
+  try {
+    console.log(req.body);
+    const user = await User.findOneAndUpdate(
+      { email: req.body.email },
+      req.body
+    );
+    if (user) {
+      res.send("User updated successfuly");
+    } else {
+      throw new Error("failed to update due to invalid info");
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).send(error.message);
+  }
+});
+
+app.get("/userId", async (req, res) => {
+  try {
+    const user = await User.findById(req.body.userId);
+    if (user) {
+      console.log(user);
+      res.send(user);
+    } else {
+      throw new Error("No user found");
+    }
+  } catch (error) {
+    console.log(error.message);
+    res.status(400).send(error.message);
+  }
+});
 
 app.post("/signup", async (req, res) => {
-  const user = new User(userData);
+  const user = new User(req.body);
   try {
     const response = await user.save();
     res.send("User Registred", response);
